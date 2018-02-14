580eb5f0d8492aa5195a27a3881497c1c0a9fdfb
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-14 05:03:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(x)
   if x % 15 == 0
     return "FizzBuzz"
-  elseif x % 3 == 0
+  else if x % 3 == 0
     return "Fizz"
-  elseif x % 5 == 0
+  else if x % 5 == 0
     return "Buzz"
   else
     return nil
